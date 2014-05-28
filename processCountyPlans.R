4e755701497cc0f29128d23715be26f8cb9c108a
RONALD LUMP <ronaldlump@RONALDs-MacBook-Air.local>
2014-05-28 17:46:57 -0500

    process by county

--- processCountyPlans.R
+++ processCountyPlans.R
@@ -3,14 +3,18 @@
 
 processCountyPlans <- function(ctyplans) {
   populousCounties[[ctyplans[1,"State"]]] -> popCounties
-  ctybronze <- ctyplans[which(ctyplans$Metal.Level=="Bronze" & !is.na(charmatch(ctyplans[10,2],popCounties$CTYNAME))),]
+  ctybronze <- ctyplans[which(ctyplans$Metal.Level=="Bronze" & !is.na(charmatch(ctyplans[,2],popCounties$CTYNAME))),]
+  if (nrow(ctybronze) != 0) {
+  #ctybronze <- ctyplans[which(ctyplans$Metal.Level=="Bronze"),]
   ctybronze[order(ctybronze$Premium.Adult.Individual.Age.21),] -> ctybronze
   ctybrnzprc <- ctybronze[,"Premium.Adult.Individual.Age.21"]
   ctybrnznumplans <- length(ctybronze[,"Premium.Adult.Individual.Age.21"])
   numCarriers <- length(unique(ctybronze[,"Issuer.Name"]))
   competitiveCutoff <- min(ctybrnzprc) + sd(ctybrnzprc)
   competitiveCarriers <- ctybronze[which(ctybronze$Premium.Adult.Individual.Age.21<competitiveCutoff),"Issuer.Name"]
-  c(numCarriers,
+  
+  c(nrow(ctybronze),
+    numCarriers,
     median(ctybrnzprc),
     sd(ctybrnzprc),
     max(ctybrnzprc),
@@ -19,12 +23,15 @@ processCountyPlans <- function(ctyplans) {
     ctybrnzprc,
     unique(competitiveCarriers)
   )
+  }
+  
 }
 
 processStatePlans <- function(stateplans) {
   
   by(stateplans,stateplans$County,processCountyPlans) -> cty
-  cty
+  lapply(cty, function(x) { if (!is.null(x[[1]])) x[[1]] } ) -> fff
+  fff[!sapply(fff, is.null)]
 }
 
 summaryStatePlans <-function() {
