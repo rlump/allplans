719408f75cf3a0d15c95a4873037b55e8c0932a6
RONALD LUMP <ronaldlump@RONALDs-MacBook-Air.local>
2014-05-28 16:37:06 -0500

    process by county

--- processCountyPlans.R
+++ processCountyPlans.R
@@ -1,12 +1,34 @@
 
-
+# processCounties(counties) -> populousCounties
 
 processCountyPlans <- function(ctyplans) {
-  
-  
+  populousCounties[[ctyplans[1,"State"]]] -> popCounties
+  ctybronze <- ctyplans[which(ctyplans$Metal.Level=="Bronze" & !is.na(charmatch(ctyplans[10,2],popCounties$CTYNAME))),]
+  ctybronze[order(ctybronze$Premium.Adult.Individual.Age.21),] -> ctybronze
+  ctybrnzprc <- ctybronze[,"Premium.Adult.Individual.Age.21"]
+  ctybrnznumplans <- length(ctybronze[,"Premium.Adult.Individual.Age.21"])
+  numCarriers <- length(unique(ctybronze[,"Issuer.Name"]))
+  competitiveCutoff <- min(ctybrnzprc) + sd(ctybrnzprc)
+  competitiveCarriers <- ctybronze[which(ctybronze$Premium.Adult.Individual.Age.21<competitiveCutoff),"Issuer.Name"]
+  c(numCarriers,
+    median(ctybrnzprc),
+    sd(ctybrnzprc),
+    max(ctybrnzprc),
+    min(ctybrnzprc),
+    ctybrnznumplans,
+    ctybrnzprc,
+    unique(competitiveCarriers)
+  )
 }
 
 processStatePlans <- function(stateplans) {
   
+  by(stateplans,stateplans$County,processCountyPlans) -> cty
+  cty
+}
+
+summaryStatePlans <-function() {
+  #plans <- read.csv("out",colClasses = "character")
+  by(plans,plans$State,processStatePlans)
   
 }
